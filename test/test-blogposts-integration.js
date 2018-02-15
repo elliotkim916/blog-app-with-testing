8284f73d9ce7f3a1ad96fb470a78d3c78303a011
Elliot Kim <elliotkim916@gmail.com>
2018-02-14 18:38:29 -0800

    able to seed database with data

--- test/test-blogposts-integration.js
+++ test/test-blogposts-integration.js
@@ -11,3 +11,39 @@ const expect = chai.expect;
 const {BlogPost} = ('../models');
 const {app, runServer, closeServer} = ('../server');
 const {TEST_DATABASE_URL} = ('../config');
+
+// seed the database
+function seedBlogPostData() {
+    console.info('seeding blog post data');
+    const seedData = [];
+
+    for (let i=1; i<=5; i++) {
+        seedData.push(generateBlogPostData());
+    }
+
+    return BlogPost.insertMany(seedData);
+}
+
+// to generate fake titles
+function generateTitle() {
+    const title = [
+        'yesterday', 
+        'today', 
+        'tomorrow', 
+        'next', 
+        'never'
+    ];
+    return title[Math.floor(Math.random() * title.length)];
+}
+
+// to generate fake blog posts
+function generateBlogPostData() {
+    return {
+        author: {
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName()
+        },
+        title: generateTitle(),
+        content: faker.lorem.paragraph()
+    }
+}
