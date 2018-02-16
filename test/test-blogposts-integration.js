203eb3442183a0793aff815a02c1c8c0222f0fb3
Elliot Kim <elliotkim916@gmail.com>
2018-02-15 16:30:05 -0800

    able to test for GET requests

--- test/test-blogposts-integration.js
+++ test/test-blogposts-integration.js
@@ -5,13 +5,13 @@ const faker = require('faker');
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-
 const expect = chai.expect;
 
-const {BlogPost} = ('../models');
-const {app, runServer, closeServer} = ('../server');
-const {TEST_DATABASE_URL} = ('../config');
+const {BlogPost} = require('../models');
+const {app, runServer, closeServer} = require('../server');
+const {TEST_DATABASE_URL} = require('../config');
 
+chai.use(chaiHttp);
 // seed the database
 function seedBlogPostData() {
     console.info('seeding blog post data');
@@ -56,7 +56,8 @@ function tearDownDb() {
 }
 
 describe('Blog Post API Resource', function() {
-    
+// in testing, mocha is expecting promises!
+
     // starts server with test database url
     before(function() {
         return runServer(TEST_DATABASE_URL);
@@ -76,4 +77,50 @@ describe('Blog Post API Resource', function() {
     after(function() {
         return closeServer();
     });
+});
+
+describe('GET endpoint', function() {
+    it('should return all blog posts', function() {
+        let res;
+        return chai.request(app)
+            .get('/posts')
+            .then(function(_res) {
+                res = _res;
+                expect(res).to.have.status(200);
+                expect(res.body).to.have.length.of.at.least(1);
+                return BlogPost.count();
+            })
+            .then(function(count) {
+                expect(res.body).to.have.length.of(count);
+            });
+    });
+
+    it('should return posts with the right fields', function() {
+     // Strategy: Get back all restaurants, and ensure they have expected keys
+     let resBlogPost;
+
+     return chai.request(app)
+        .get('/posts')
+        .then(function(res) {
+            expect(res).to.have.status(200);
+            expect(res).to.be(json);
+            expect(res.body).to.have.length.of.at.least(1);
+            expect(res.body).to.be.a('array');
+
+            res.body.forEach(function(post) {
+                expect(post).to.be.a('object');
+                expect(post).to.include.keys('title', 'content', 'author');
+            });
+
+            resBlogPost = res.body[0];
+            return BlogPost.findById(resBlogPost.id);
+        })
+        .then(function(post) {
+            expect(resBlogPost.title).to.equal(post.title);
+            expect(resBlogPost.content).to.equal(post.content);
+            expect(resBlogPost.author.firstName).to.equal(post.author.firstName);
+            expect(resBlogPost.author.lastName).to.equal(post.author.lastName);
+        });   
+    });
+
 });
